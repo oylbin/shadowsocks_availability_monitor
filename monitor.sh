8d3a5a58595efb21b2d9a7193135839fa72aaa9b
oylbin <oylbin@gmail.com>
2021-11-16 14:42:50 +0800

    add SS_OBFS_OPTS env param

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,28 @@
-docker run --rm --name ss_monitor -d oylbin/ss_with_obfs:latest ss-tunnel \
+function check_param {
+    for i in $@
+    do
+        if [[ ! "${!i}" ]]
+        then
+            echo "need env param: $i"
+            exit 1
+        fi
+    done
+    return 0
+}
+
+check_param SS_SERVER SS_PORT SS_METHOD SS_PASSWORD
+
+if [ "$SS_OBFS_OPTS" = "" ] ; then
+    docker run --rm --name ss_monitor -d oylbin/ss_with_obfs:latest ss-tunnel \
         -s $SS_SERVER -p $SS_PORT -m "$SS_METHOD" -k "$SS_PASSWORD" \
-        --plugin obfs-local --plugin-opts "obfs=http;obfs-host=ouyang.me" \
         -b 0.0.0.0 -l 9527 -L www.google.com:80 -v
+else
+    docker run --rm --name ss_monitor -d oylbin/ss_with_obfs:latest ss-tunnel \
+        -s $SS_SERVER -p $SS_PORT -m "$SS_METHOD" -k "$SS_PASSWORD" \
+        --plugin obfs-local --plugin-opts "$SS_OBFS_OPTS" \
+        -b 0.0.0.0 -l 9527 -L www.google.com:80 -v
+fi
+
 docker exec ss_monitor bash -c \
     'cat <(echo -e "HEAD / HTTP/1.0\nHost: www.google.com\n\n") <(sleep 5) | nc -w 10 127.0.0.1 9527' > /tmp/ss_monitor.log
 docker stop ss_monitor
